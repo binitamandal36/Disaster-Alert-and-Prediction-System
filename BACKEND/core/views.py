0c6c8b6ec7e0eba6e35dbc88cfa42831ee08b75b
Binita Kumari Mandal <mbinita336@gmail.com>
2025-12-26 06:57:03 +0545

    adding disaster in django administration

--- BACKEND/core/views.py
+++ BACKEND/core/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Disaster
 
-def home(request):
-    return HttpResponse("Welcome to Disaster Alert and Prediction System")
+def disaster_list(request):
+    disasters = Disaster.objects.all().order_by('-date_reported')   
+    return render(request, 'core/disaster_list.html', {
+        'disasters': disasters
+    })
+
+
+#Disaster.objects.all(): Fetches all disaster records from DB
+#order_by('-date_reported'): Shows latest disaster first
