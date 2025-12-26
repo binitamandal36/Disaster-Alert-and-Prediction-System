f7d1d94de8d3a59ebee0a36847bb493580cc9391
Binita Kumari Mandal <mbinita336@gmail.com>
2025-12-26 08:22:03 +0545

    url-mapped

--- BACKEND/disaster_alert/urls.py
+++ BACKEND/disaster_alert/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import disaster_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('disasters/', disaster_list,name='disaster_list'),
 ]
+
